dea889fdaacc64f75af0b57c91d37ec059079d5f
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2016-02-11 16:33:35 +0100

    Modifies scraper according to previous commits.

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,12 @@
 require "tty"
 require "pastel"
 require "optitron"
-require "tty"
 require "httparty"
 require "sequel"
 Sequel.connect("postgres:///postgres")
 require_relative "lib/scraper"
 
-REMOVED = [2424,2388,2376,2353,2336,2251,2186,2059,1978,1924,1794,978,963,962,960,954,902,880,828,797,776,722,844]
+REMOVED = [2621,2607,2563,2550,2529,2516,2515,2503,2462,2461,2441,2424,2388,2376,2353,2336,2251,2186,2059,1978,1924,1794,978,963,962,960,954,902,880,828,797,776,722,844,662,650,641,607,606,582,548,538,537,529,528,508,501,481,450,448,422,403,386,380,358,356,346,345,322,321,308,283,276,269,264,253,207,177,161,130,102,88,61,47,21,20,12]
 
 class Scraper < Optitron::CLI
   def parse_results(results)
@@ -28,43 +27,6 @@ class Scraper < Optitron::CLI
     end
   end
 
-  def retrieve_arguments(debate)
-    url = "http://arguman.org/api/v1/arguments/#{debate.id}/premises"
-    response = HTTParty.get(url)
-    res = response.parsed_response
-    puts debate.title
-    res.each do |arg|
-      user = User.find_or_create(:id=>arg["user"]["id"], :username=>arg["user"]["username"])
-      next if Argument[arg["id"]]
-      Argument.create do |a|
-        a.id = arg["id"]
-        a.text = arg["text"]
-        a.type = arg["premise_type"]
-        a.user = user
-        a.parent = Argument[arg["parent"]]
-        a.debate = Debate[debate.id]
-      end
-      puts arg["text"]
-    end
-  end
-
-  def build_apxd_file(debate)
-    File.open("debates_apxd/#{debate.id}.apxd", "w") do |file|
-      file.write("arg(root).\n")
-      file.write("text(root, #{debate.title}).\n\n")
-      debate.arguments.each do |arg|
-        file.write("arg(#{arg.id}).\n")
-        file.write("text(#{arg.id}, #{arg.text}).\n")
-        parent = (arg.parent ? arg.parent.id : "root")
-        if arg.type == 1
-          file.write("support(#{arg.id}, #{parent}).\n\n")
-        else
-          file.write("att(#{arg.id}, #{parent}).\n\n")
-        end
-      end
-    end
-  end
-
   desc "Retrieve the new debates (but not the arguments)"
   arg_types :numeric
   def get_debates(up_to_page=20)
@@ -75,24 +37,25 @@ class Scraper < Optitron::CLI
       json = response.parsed_response
       parse_results(json["results"])
       url = json["next"]
-      break if url.nil? || url == "http://arguman.org/api/v1/arguments/?page=#{up_to_page}"
+      break if url.nil? || url == "http://arguman.org/api/v1/arguments/?page=#{up_to_page+1}"
     end
   end
 
   desc "Retrieve arguments of all debates"
   def get_arguments
     Debate.dataset.all.each do |d|
-      retrieve_arguments(d)
+      d.retrieve_arguments(true)
     end
   end
 
   desc "Build apxd files from the debates"
   def build_apxd
+    dir    = "debates_apxd"
     nb_deb = Debate.dataset.count
     pastel = Pastel.new
     bar = TTY::ProgressBar.new("Creating files [:bar] :current/:total", :total=>nb_deb, :complete=>pastel.on_green("-"), :incomplete=>pastel.on_red("-"))
     Debate.dataset.each do |d|
-      build_apxd_file(d)
+      d.build_apx_file(dir)
       bar.advance(1)
     end
   end
@@ -112,6 +75,74 @@ class Scraper < Optitron::CLI
     end
     table = TTY::Table.new :rows=>val
     puts table.render :ascii, :alignment=>[:left, :right]
+  end
+
+  desc "Create files and compute similarity distance for a debate"
+  def similitude(debate_id, siblings_only=false)
+    arg_ids = []
+    STDERR.puts "Creating file"
+    File.open("#{debate_id}.txt", "w") do |file|
+      d = Debate[debate_id.to_i]
+      if siblings_only
+        arg_ids = d.cross_siblings(file)
+      else
+        arg_ids = d.cross_all_args(file)
+      end
+    end
+    STDERR.puts "Features"
+    %x{python takelab_simple_features.py #{debate_id}.txt > "#{debate_id}-feat.txt"}
+    STDERR.puts "Predict"
+    %x{svm-predict #{debate_id}-feat.txt model.txt #{debate_id}-pred.txt}
+    STDERR.puts "Post process"
+    %x{python postprocess_scores.py #{debate_id}.txt #{debate_id}-pred.txt}
+    values = Array.new
+    g = Graph.new
+    STDERR.puts "Sorting"
+    File.open("#{debate_id}-pred.txt", "r") do |file|
+      ind = 0
+      while(line = file.gets)
+        values << [ind, line.to_f]
+        id_ind = arg_ids[ind]
+        g.add_edge(id_ind[0], id_ind[1], line.to_f)
+        ind += 1
+      end
+    end
+    value_hash = g.optimize
+    values.zip(arg_ids).each do |val, ids|
+      new_val = value_hash[ids[0..1].minmax.join("_")]
+      val << new_val*5.0 << ids[2]
+    end
+    values.sort! {|a,b| b[2] <=> a[2]}
+    values.each do |val|
+      puts "#{val[3]} #{val[2]} #{val[1]}"
+    end
+  end
+
+  desc "Create files and compute similarity distance for a debate using Cortical API"
+  def cortical_sim(debate_id, siblings_only=false)
+    arg_ids = []
+    d = Debate[debate_id.to_i]
+    if siblings_only
+      arg_ids = d.cross_siblings
+    else
+      arg_ids = d.cross_all_args
+    end
+    STDERR.puts "Creating API call JSON"
+
+    json_arr = []
+    arg_ids.lazy.map(&:last).each do |args|
+      arg1, arg2 = args.split("#")
+      json_arr << [{"text"=>arg1}, {"text"=>arg2}]
+    end
+    response = HTTParty.post("http://api.cortical.io:80/rest/compare/bulk?retina_name=en_associative", :body => json_arr.to_json, :headers => {"api-key" => "PLACEHOLDER", "Content-Type" => "application/json" })
+
+    json = response.parsed_response
+    ind_with_index = json.map {|h| h["cosineSimilarity"]}.zip(arg_ids.map(&:last))
+    sorted = ind_with_index.sort! {|a,b| b[0] <=> a[0]}
+    sorted.each do |val, arg|
+      arg1, arg2 = arg.split("#")
+      puts "#{arg1} // #{arg2} #{val}"
+    end
   end
 end
 
